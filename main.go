ca6add8f04f874070220fb7faaa6e2abbec31c13
Salvador Guzman <guzmansalv@gmail.com>
2018-10-18 19:14:56 -0700

    get max pages

--- main.go
+++ main.go
@@ -1,1 +1,38 @@
-package go_get_channels
+package main
+
+import (
+    "fmt"
+    "github.com/PuerkitoBio/goquery"
+    "net/http"
+    "strings"
+    "strconv"
+)
+
+func main() {
+    // Request the HTML page.
+    res, err := http.Get("https://dbase.tube/chart/channels/subscribers/all")
+    if err != nil {
+        panic(err)
+    }
+    defer res.Body.Close()
+    if res.StatusCode != 200 {
+        panic(fmt.Sprintf("status code error: %d %s", res.StatusCode, res.Status))
+    }
+
+    doc, err := goquery.NewDocumentFromReader(res.Body)
+    if err != nil {
+        panic(err)
+    }
+
+    // Find the review items
+    links := doc.Find("a[href^=\"/chart/channels/subscribers/all?page=\"]")
+    pageCountStr := links.Last().Get(0).Attr[0].Val
+    countIdx := strings.Index(pageCountStr, "=")
+    countStr := pageCountStr[countIdx+1:len(pageCountStr)]
+
+    if count, err := strconv.Atoi(countStr); err == nil {
+        fmt.Println(count)
+    } else {
+        panic(err)
+    }
+}
